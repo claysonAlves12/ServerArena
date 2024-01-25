d347b15afbd06deca924d08b2b0cc3d2995889df
claysonAlves12 <128482372+claysonAlves12@users.noreply.github.com>
2024-01-25 11:29:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -583,3 +583,8 @@ app.get('/politica-de-privacidade', (req, res) => {
 app.use((req, res) => {
   res.render('404');
 });
+
+const PORT = process.env.PORT || 3000; 
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
